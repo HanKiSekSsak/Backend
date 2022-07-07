a772950df415b1730a1dd7382d41c5806f98245c
luculent126 <79980546+luculent126@users.noreply.github.com>
2022-07-07 15:47:28 +0900

    done

--- ideaton/account/views.py
+++ ideaton/account/views.py
@@ -2,33 +2,31 @@ from django.shortcuts import redirect, render
 from django.contrib import auth
 from django.contrib.auth.models import User
 
-# Create your views here.
+# 로그인
 def login(request):
     if request.method == 'POST':
-        userid = request.POST['user-id']
-        pwd = request.POST['user-password']
-        user = auth.authenticate(request, username = userid, password = pwd)
+        id = request.POST.get('user_id')
+        pwd = request.POST.get('user_pw')
+        user = auth.authenticate(request, username = id, password = pwd)
         if user is not None:
             auth.login(request, user)
-            # tag.html
-            return render(request, 'tag.html')
+            return redirect('tag')
         else:
-            # register.html
-            return render(request, 'register.html')
+            return redirect('tag')
 
-    # get요청이 들어오면 login form을 담고있는 login.html을 띄워준다.
     else:
-        # login.html
         return render(request, 'login.html')
 
+# 로그아웃
 def logout(request):
     auth.logout(request)
     return redirect('home')
 
+# 회원가입
 def register(request):
     if request.method == 'POST':
-        if request.POST['password'] == request.POST['confirm']:
-            user = User.objects.create_user(username = request.POST['username'], password = request.POST['password'])
+        if request.POST.get('password') == request.POST.get('confirm'):
+            user = User.objects.create_user(username = request.POST['name'], password = request.POST['password'])
             auth.login(request, user)
             return redirect('tag')
     return render(request, 'register.html')
--- ideaton/ideaton/urls.py
+++ ideaton/ideaton/urls.py
@@ -22,20 +22,20 @@ urlpatterns = [
     path('register/', account_views.register, name = 'register'),
 
     # tag url 생성
-    path('tag_list/', views.tag, name = 'tag'),
+    path('tag/', views.tag, name = 'tag'),
 
     # tag_list에서 찌개 url 생성
-    path('tag_list/jjigae/', views.jjigae, name = 'jjigae'),
+    path('tag/jjigae/', views.jjigae, name = 'jjigae'),
 
     # tag_list에서 국 url 생성
-    path('tag_list/soup/', views.soup, name = 'soup'),
+    path('tag/soup/', views.soup, name = 'soup'),
 
     # tag_list에서 찜 url 생성
-    path('tag_list/steam/', views.steam, name = 'steam'),
+    path('tag/steam/', views.steam, name = 'steam'),
 
     # tag_list에서 구이 url 생성
-    path('tag_list/roast/', views.roast, name = 'roast'),
+    path('tag/roast/', views.roast, name = 'roast'),
 
     # tag_list에서 탕 url 생성
-    path('tag_list/tang/', views.tang, name = 'tang'),
+    path('tag/tang/', views.tang, name = 'tang'),
 ]
